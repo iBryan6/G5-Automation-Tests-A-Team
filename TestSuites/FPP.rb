5c8c432e63b6972ef8e7326dee4cc68f49961e82
iBryan6 <Dennis_Bryan@hotmail.com>
2019-09-30 16:09:14 -0400

    created FPP Test Suite file

---
+++ TestSuites/FPP.rb
@@ -0,0 +1,9 @@
+require "rubygems"
+require "webdrivers"
+
+#CHANGEABLE VARIABLES
+##CLIENT AND LOCATION
+@cms = 'https://content-management-system-content-staging.g5devops.com/clients/g5-c-5dtat2mbv-chewbacca-sd-mf-ssl/websites'
+@locationName = 'Floor Plans Plus'
+
+
